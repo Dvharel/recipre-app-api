27ed000fff2bfd71580baead66cb5d5ed67217e9
Dvharel <dvharel21@gmail.com>
2025-09-25 18:46:47 +0300

    Fix flake8 issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,12 +1,11 @@
-"""
-Calculator functions
-"""
+"""Calculator functions."""
 
 
 def add(x, y):
     """Add x and y and return result."""
     return x + y
 
+
 def subtract(x, y):
-    """Subtract x from y and return result."""
+    """Subtract y from x and return result."""
     return y - x
--- app/app/test.py
+++ app/app/test.py
@@ -1,7 +1,7 @@
 """
 Sample tests
 """
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase  # noqa: F401
 
 from app import calc
 
